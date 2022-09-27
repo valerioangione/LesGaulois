eea55f997bf6b429e2639b021f55a36daa285d4b
angione valerio <ngv3871a@ens.fsi>
2022-09-27 19:50:49 +0200

    Initulé des modifications

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,45 @@
+package personnages;
+
+public class Gaulois {
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	public int getForce() {
+		return force;
+	}
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	private void parler(String texte) {
+		System.out.println(prendreParole()+"«" + texte + "»");
+	}
+	
+	private String prendreParole() {
+		return "Le gaulois" + nom + ":";
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de "+ romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+
+	
+	
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
+
